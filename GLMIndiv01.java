baffa282e87fd3944271ebc0a458e3633c2ea5a4
Levert65 <43524361+Levert65@users.noreply.github.com>
2018-09-23 22:47:54 +0200

    Add files via upload

---
+++ GLMIndiv01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glm.indiv01;
+
+/**
+ *
+ * @author Manuel
+ */
+public class GLMIndiv01 {
+
+    public static void main(String[] args) {
+      
+        int numero; //primero, declaro la variable entera y le asigno un número
+        numero=13; //segundo, hay que darle un valor a la variable
+        System.out.println(numero); //y por último, muestro la variable por pantalla
+        System.out.println("Mi nombre es Manuel Gómez López"); //muestro mi nombre por pantalla
+    
+    
+    
+    
+    
+    }
+    
+    
+    }
